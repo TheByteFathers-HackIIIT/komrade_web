6b74c72fa08c87ca017a973ca11a6672e04f9269
The-Coder-Kishor <talakishor47@gmail.com>
2024-01-20 22:55:35 +0530

    Removed shoelace

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,3 @@
-import '@shoelace-style/shoelace/dist/themes/light.css';
-import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path';
-
-setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.12.0/cdn/');
-
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -33,7 +28,6 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <sl-button>Click me</sl-button>
     </>
   )
 }
