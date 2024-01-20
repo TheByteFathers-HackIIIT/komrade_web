281de4816c6ecea913d75674ae4f2baa12b62dca
The-Coder-Kishor <talakishor47@gmail.com>
2024-01-21 01:02:13 +0530

    Modify SignIn functionality

--- src/App.tsx
+++ src/App.tsx
@@ -27,24 +27,8 @@ function App() {
         containerClassName="overflow-auto"
       />
       <Routes>
-        <Route path="/auth/signin" element={<SignIn />} />
-        <Route path="/auth/signup" element={<SignUp />} />
         <Route element={<DefaultLayout />}>
-          <Route index element={<ECommerce />} />
-          {routes.map((routes, index) => {
-            const { path, component: Component } = routes;
-            return (
-              <Route
-                key={index}
-                path={path}
-                element={
-                  <Suspense fallback={<Loader />}>
-                    <Component />
-                  </Suspense>
-                }
-              />
-            );
-          })}
+          <Route index element={<SignIn />} />
         </Route>
       </Routes>
     </>
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,8 +9,14 @@ const Settings = lazy(() => import('../pages/Settings'));
 const Tables = lazy(() => import('../pages/Tables'));
 const Alerts = lazy(() => import('../pages/UiElements/Alerts'));
 const Buttons = lazy(() => import('../pages/UiElements/Buttons'));
+const SignIn = lazy(() => import('../pages/Authentication/SignIn'));
 
 const coreRoutes = [
+  {
+    path: '/signin',
+    title: 'Sign In',
+    component: SignIn,
+  },
   {
     path: '/calendar',
     title: 'Calender',
