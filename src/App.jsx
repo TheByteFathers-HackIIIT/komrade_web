371c5e3c913cdeaade589b230bb9182bcfbfa153
The-Coder-Kishor <talakishor47@gmail.com>
2024-01-20 23:37:02 +0530

    Add react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Navbar from './components/Navbar';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Navbar />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+// Navbar.js
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+
+  return (
+  <nav>
+  </nav>
+  );
+};
+
+export default Navbar;
