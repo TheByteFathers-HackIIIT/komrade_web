0a89180ddb6fb85629cb0d988c0b840d4cba8660
The-Coder-Kishor <talakishor47@gmail.com>
2024-01-20 22:40:06 +0530

    Add shoelace library

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
+import '@shoelace-style/shoelace/dist/themes/light.css';
+import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path';
+
+setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.12.0/cdn/');
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
