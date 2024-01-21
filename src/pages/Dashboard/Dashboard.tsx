3197f7c621becbead5a3a85e5decf7d394d117e5
Mohit Singh <unignoramus@gmail.com>
2024-01-21 17:13:37 +0530

    update dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,12 +1,12 @@
-import CardFour from '../../components/CardFour.tsx';
-import CardOne from '../../components/CardOne.tsx';
-import CardThree from '../../components/CardThree.tsx';
-import CardTwo from '../../components/CardTwo.tsx';
-import ChartOne from '../../components/ChartOne.tsx';
-import ChartThree from '../../components/ChartThree.tsx';
-import ChartTwo from '../../components/ChartTwo.tsx';
-import ChatCard from '../../components/ChatCard.tsx';
-import MapOne from '../../components/MapOne.tsx';
+// import CardFour from '../../components/CardFour.tsx';
+// import CardOne from '../../components/CardOne.tsx';
+// import CardThree from '../../components/CardThree.tsx';
+// import CardTwo from '../../components/CardTwo.tsx';
+// import ChartOne from '../../components/ChartOne.tsx';
+// import ChartThree from '../../components/ChartThree.tsx';
+// import ChartTwo from '../../components/ChartTwo.tsx';
+// import ChatCard from '../../components/ChatCard.tsx';
+// import MapOne from '../../components/MapOne.tsx';
 import TableOne from '../../components/TableOne.tsx';
 
 const Dashboard = () => {
