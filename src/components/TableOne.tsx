d19a657bf33ccf9d5f9f678b4646c22c26ace8a6
The-Coder-Kishor <talakishor47@gmail.com>
2024-01-21 18:58:37 +0530

    Final

--- src/components/TableOne.tsx
+++ src/components/TableOne.tsx
@@ -1,10 +1,29 @@
-import BrandOne from '../images/brand/brand-01.svg';
-import BrandTwo from '../images/brand/brand-02.svg';
-import BrandThree from '../images/brand/brand-03.svg';
-import BrandFour from '../images/brand/brand-04.svg';
-import BrandFive from '../images/brand/brand-05.svg';
+import { useEffect, useState } from 'react';
+import { apiUrl } from '../App';
+import { genheaders } from '../js/utils';
 
-const MyOrders = () => {
+const MyOrders: React.FC = () => {
+  const [orders, setOrders] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try{
+        const response = await fetch(
+          apiUrl+'/orderhistory',
+          {
+            method: 'GET',
+            headers: genheaders(),
+            mode: 'cors',
+          });
+          const data = await response.json();
+          setOrders(data);
+      }
+      catch(error){
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
       <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
@@ -30,15 +49,38 @@ const MyOrders = () => {
           </div>
           <div className="hidden p-2.5 text-center sm:block xl:p-5">
             <h5 className="text-sm font-medium uppercase xsm:text-base">
-              Status
+            Orderer Phone Number
             </h5>
           </div>
           <div className="hidden p-2.5 text-center sm:block xl:p-5">
             <h5 className="text-sm font-medium uppercase xsm:text-base">
-              Komrade
+              Completed Status
             </h5>
           </div>
         </div>
+        {orders.map((order, index) => (
+          <div key={index} className={`grid grid-cols-3 border-b border-stroke dark:border-strokedark sm:grid-cols-5`}>
+            <div className="flex items-center gap-3 p-2.5 xl:p-5">
+              <p className="hidden text-black dark:text-white sm:block">{order['items']}</p>
+            </div>
+
+            <div className="flex items-center justify-center p-2.5 xl:p-5">
+              <p className="text-black dark:text-white">{order['canteen']}</p>
+            </div>
+
+            <div className="flex items-center justify-center p-2.5 xl:p-5">
+              <p className="text-black dark:text-white">{order['hostel']}</p>
+            </div>
+
+            <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
+            <p className="text-black dark:text-white">{order['ordererNumber']}</p>
+            </div>
+
+            <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
+              <p className="text-orange-500">{order['completed']}</p>
+            </div>
+          </div>
+        ))}
 
         <div className="grid grid-cols-3 border-b border-stroke dark:border-strokedark sm:grid-cols-5">
           <div className="flex items-center gap-3 p-2.5 xl:p-5">
